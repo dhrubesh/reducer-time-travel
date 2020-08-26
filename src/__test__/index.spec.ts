6fe49c6524d93f17a168101963fb11d5b119a501
Dhrubesh Deb Sharma <dhrubesh97@gmail.com>
2020-08-26 19:51:46 +0530

    initiat test set up

--- src/__test__/index.spec.ts
+++ src/__test__/index.spec.ts
@@ -1,5 +1,27 @@
-import { sum } from '../index'
+import { useTimeTravel } from '../index'
+import { renderHook, act } from '@testing-library/react-hooks';
 
-test('Sum 4 + 5 = 9', () => {
-  expect(sum(4, 5)).toBe(9)
+const reducer = (state: number, action: { type: string, payload: number }) => {
+  switch (action.type) {
+    case 'ADD':
+      return state + action.payload;
+    case 'SUB':
+      return state - action.payload;
+    default:
+      return state;
+  }
+}
+
+test('test reducer', () => {
+  const initialState: number = 2;
+  const { result } = renderHook(() => useTimeTravel(
+    reducer,
+    initialState
+  ));
+  const { dispatch } = result.current;
+  act(() => dispatch({ type: 'ADD', payload: 23 }))
+  act(() => dispatch({ type: 'SUB', payload: 2 }))
+  act(() => result.current.goToPast())
+  act(() => result.current.goToFuture())
+  act(() => result.current.doReset())
 })
